cefd6aaac74c30318545877cbf337a1cd68bdf53
Austin <island6614@gmail.com>
2021-01-28 01:34:22 +0800

    Create open_chrome.py

---
+++ open_chrome.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 21:46:30 2021
+
+@author: islan
+"""
+import os
+
+os.system('start chrome.exe --remote-debugging-port=9223 --user-data-dir="C:\selenum\AutomationProfile"')
